95c9f961b781e6dda975d510950f24c530eb9724
liranko27 <liranko13@gmail.com>
2022-07-27 12:29:26 +0300

    update files

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../styles/HamburgerMenu.css";
-function HamburgerMenu() {
+function HamburgerMenu({ openCart }) {
   return (
     <div className="hamburger-menu">
       <h1>Menu</h1>
       <h5>Home</h5>
       <h5>About</h5>
-      <h5>Cart</h5>
+      <h5 onClick={openCart}>Cart</h5>
       <h5>MyAccount</h5>
     </div>
   );
