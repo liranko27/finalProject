ec7914eaf58637ebbfe34de8d5d56df95f5d0013
liranko27 <liranko13@gmail.com>
2022-07-27 11:53:43 +0300

    refactor myprofile forms

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Footer from './Footer'
-import Navbar from './Navbar'
 import '../styles/MyProfile.css'
 import WishCard from './WishCard'
 import { useState } from 'react'
 import FormInput from './FormInput'
+import { inputsArr, myValues } from '../inputs/myAccountInputs'
+
 
 const def = {
     wish: false,
@@ -16,157 +16,10 @@ const def = {
 
 function MyProfile() {
     const [open, setOpen] = useState(def)
-    const [values, setValues] = useState({
-        password: {
-            oldpassword: '',
-            password: '',
-            confirmPassword: '',
-        },
-        name: {
-            firstName: '',
-            lastName: ''
-        },
-        address: {
-            city: '',
-            street: '',
-            zipcode: '',
-        },
-        email: {
-            oldemail: '',
-            email: '',
-            confirmEmail: '',
-        }
+    const [values, setValues] = useState(myValues)
+    const inputs = inputsArr()
+    inputs.password[2].pattern = values.password.password
 
-    })
-
-    const inputs = {
-        password: [
-            {
-                id: 1,
-                name: "oldpassword",
-                type: "password",
-                placeholder: "Old Password",
-                label: "Old Password",
-                errorMessage:
-                    "Password should be 8-20 chars must includ at least 1 letter , 1 number and 1 spatial char",
-                required: true,
-                pattern:
-                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$",
-            },
-            {
-                id: 2,
-                name: "password",
-                type: "password",
-                placeholder: "Password",
-                label: "Password",
-                errorMessage:
-                    "Password should be 8-20 chars must includ at least 1 letter , 1 number and 1 spatial char",
-                required: true,
-                pattern:
-                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$",
-            }, {
-                id: 3,
-                name: "confirmPassword",
-                type: "password",
-                placeholder: "Confirm Password",
-                label: "Confirm Password",
-                errorMessage: "Password dont match ",
-                required: true,
-                pattern: values.password.password,
-            },
-        ],
-        name: [
-            {
-                id: 1,
-                name: "firstName",
-                type: "text",
-                placeholder: "First Name",
-                label: "First Name",
-                errorMessage:
-                    "First name should be 3-16 chars and not inclued speatial char",
-                pattern: "^[A-Za-z0-9]{3,16}$",
-                required: true,
-            }, {
-                id: 2,
-                name: "lastName",
-                type: "text",
-                placeholder: "Last Name",
-                label: "Last Name",
-                errorMessage:
-                    "Last name should be 3-16 chars and not inclued speatial char",
-                pattern: "^[A-Za-z0-9]{3,16}$",
-                required: true,
-            }
-        ],
-        address: [
-            {
-                id: 1,
-                name: "city",
-                type: "text",
-                placeholder: "City",
-                label: "City",
-                errorMessage:
-                    "city should be 2-10 chars and not inclued speatial char or number",
-                pattern: "^[A-Za-z]{2,10}$",
-                required: true,
-            },
-            {
-                id: 2,
-                name: "street",
-                type: "text",
-                placeholder: "Street",
-                label: "Street",
-                errorMessage:
-                    "street should be 2-10 chars and not inclued speatial char or number",
-                pattern: "^[A-Za-z]{2,10}$",
-                required: true,
-            },
-            {
-                id: 3,
-                name: "zipcode",
-                type: "number",
-                placeholder: "Zip code",
-                label: "Zip code",
-                errorMessage:
-                    "zipcode should be 3-16 chars and not inclued speatial char or number",
-                pattern: "^[0-9]{4,10}$",
-                required: false,
-            }
-        ],
-        email: [
-            {
-                id: 1,
-                name: "oldemail",
-                type: "email",
-                placeholder: "Old Email",
-                label: "Old Email",
-                errorMessage: "should be a valid email",
-                required: true,
-                pattern: "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
-            },
-            {
-                id: 2,
-                name: "email",
-                type: "email",
-                placeholder: "Email",
-                label: "Email",
-                errorMessage: "should be a valid email",
-                required: true,
-                pattern: "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
-            },
-            {
-                id: 3,
-                name: "confirmEmail",
-                type: "email",
-                placeholder: "Confirm Email",
-                label: "Confirm Email",
-                errorMessage: "should be a valid email",
-                required: true,
-                pattern: "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
-            }
-        ],
-    }
-    console.log(values)
     const handleChange = (e, name) => {
         setValues({ ...values, [name]: { ...values[name], [e.target.name]: e.target.value } });
     };
---
+++ src/inputs/myAccountInputs.js
@@ -0,0 +1,153 @@
+export function inputsArr() {
+    return {
+        password: [
+            {
+                id: 1,
+                name: "oldpassword",
+                type: "password",
+                placeholder: "Old Password",
+                label: "Old Password",
+                errorMessage:
+                    "Password should be 8-20 chars must includ at least 1 letter , 1 number and 1 spatial char",
+                required: true,
+                pattern:
+                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$",
+            },
+            {
+                id: 2,
+                name: "password",
+                type: "password",
+                placeholder: "Password",
+                label: "Password",
+                errorMessage:
+                    "Password should be 8-20 chars must includ at least 1 letter , 1 number and 1 spatial char",
+                required: true,
+                pattern:
+                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$",
+            }, {
+                id: 3,
+                name: "confirmPassword",
+                type: "password",
+                placeholder: "Confirm Password",
+                label: "Confirm Password",
+                errorMessage: "Password dont match ",
+                required: true,
+                pattern: '',
+            },
+        ],
+        name: [
+            {
+                id: 1,
+                name: "firstName",
+                type: "text",
+                placeholder: "First Name",
+                label: "First Name",
+                errorMessage:
+                    "First name should be 3-16 chars and not inclued speatial char",
+                pattern: "^[A-Za-z0-9]{3,16}$",
+                required: true,
+            }, {
+                id: 2,
+                name: "lastName",
+                type: "text",
+                placeholder: "Last Name",
+                label: "Last Name",
+                errorMessage:
+                    "Last name should be 3-16 chars and not inclued speatial char",
+                pattern: "^[A-Za-z0-9]{3,16}$",
+                required: true,
+            }
+        ],
+        address: [
+            {
+                id: 1,
+                name: "city",
+                type: "text",
+                placeholder: "City",
+                label: "City",
+                errorMessage:
+                    "city should be 2-10 chars and not inclued speatial char or number",
+                pattern: "^[A-Za-z]{2,10}$",
+                required: true,
+            },
+            {
+                id: 2,
+                name: "street",
+                type: "text",
+                placeholder: "Street",
+                label: "Street",
+                errorMessage:
+                    "street should be 2-10 chars and not inclued speatial char or number",
+                pattern: "^[A-Za-z]{2,10}$",
+                required: true,
+            },
+            {
+                id: 3,
+                name: "zipcode",
+                type: "number",
+                placeholder: "Zip code",
+                label: "Zip code",
+                errorMessage:
+                    "zipcode should be 3-16 chars and not inclued speatial char or number",
+                pattern: "^[0-9]{4,10}$",
+                required: false,
+            }
+        ],
+        email: [
+            {
+                id: 1,
+                name: "oldemail",
+                type: "email",
+                placeholder: "Old Email",
+                label: "Old Email",
+                errorMessage: "should be a valid email",
+                required: true,
+                pattern: "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
+            },
+            {
+                id: 2,
+                name: "email",
+                type: "email",
+                placeholder: "Email",
+                label: "Email",
+                errorMessage: "should be a valid email",
+                required: true,
+                pattern: "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
+            },
+            {
+                id: 3,
+                name: "confirmEmail",
+                type: "email",
+                placeholder: "Confirm Email",
+                label: "Confirm Email",
+                errorMessage: "should be a valid email",
+                required: true,
+                pattern: "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
+            }
+        ],
+    }
+}
+
+
+export const myValues = {
+    password: {
+        oldpassword: '',
+        password: '',
+        confirmPassword: '',
+    },
+    name: {
+        firstName: '',
+        lastName: ''
+    },
+    address: {
+        city: '',
+        street: '',
+        zipcode: '',
+    },
+    email: {
+        oldemail: '',
+        email: '',
+        confirmEmail: '',
+    }
+
+}
