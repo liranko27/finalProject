f7cae7e264e8e3762e916a38aef9d2ff8f49ed58
liranko27 <liranko13@gmail.com>
2022-07-27 12:02:24 +0300

    login form inputs data refactor

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,37 +3,12 @@ import FormInput from "./FormInput";
 import { useState } from "react";
 import "../styles/MyForm.css";
 import "../styles/LoginForm.css";
+import { loginInputs, loginValues } from '../inputs/loginFormInputs'
 
 function LoginForm({ open, setOpen, login, setLogin }) {
-  const [values, setValues] = useState({
-    username: "",
-    password: "",
-  });
-  const inputs = [
-    {
-      id: 1,
-      name: "username",
-      type: "text",
-      placeholder: "Username",
-      label: "Username",
-      errorMessage:
-        "username should be 3-16 chars and not inclued speatial char",
-      pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "password",
-      type: "password",
-      placeholder: "Password",
-      label: "Password",
-      errorMessage:
-        "Password should be 8-20 chars must includ at least 1 letter , 1 number and 1 spatial char",
-      required: true,
-      pattern:
-        "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$",
-    },
-  ];
+  const [values, setValues] = useState(loginValues);
+  const inputs = loginInputs();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setOpen(false);
