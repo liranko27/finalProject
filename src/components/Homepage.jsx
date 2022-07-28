4de3b2baa3aba342a613c8faa005ee31c78d571b
liranko27 <liranko13@gmail.com>
2022-07-28 16:25:52 +0300

    render categories on sidebar with new componenet

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,15 +3,17 @@ import '../styles/Homepage.css'
 import ProductCard from './ProductCard'
 import { getData } from '../DAL/api'
 import { useState, useEffect } from 'react'
-
+import categories from "../DAL/categories.json"
+import Category from './Category'
 
 function Homepage() {
     const [products, setProducts] = useState([])
+    // console.log(categories)
     useEffect(() => {
         getData().then(setProducts)
     }, [])
-    console.log(products)
-    function sortByPrice(key, dir) {
+
+    function sortProducts(key, dir) {
         setProducts([...products.sort((a, b) => {
             if (a[key] > b[key]) {
                 return -1 * dir
@@ -35,43 +37,24 @@ function Homepage() {
                     <span>Sory By:</span>
                 </div>
                 <div>
-                    <input type="radio" name="sort" id="LTH" onClick={() => sortByPrice('unitPrice', -1)} />
-                    <label htmlFor='LTH'>Low to high</label>
-                    <input type="radio" name="sort" id="HTL" onClick={() => sortByPrice('unitPrice', 1)} />
-                    <label htmlFor='HTL'>High to low</label>
+                    <input type="radio" name="sort" id="LTH" onClick={() => sortProducts('unitPrice', -1)} />
+                    <label htmlFor='LTH'>Price Low to high</label>
+                    <input type="radio" name="sort" id="HTL" onClick={() => sortProducts('unitPrice', 1)} />
+                    <label htmlFor='HTL'>Price High to low</label>
                 </div>
                 <div>
-                    <input type="radio" name="sort" id="newest" onClick={() => sortByPrice('published', 1)} />
+                    <input type="radio" name="sort" id="newest" onClick={() => sortProducts('published', 1)} />
                     <label htmlFor='newest'>Newest product</label>
-                    <input type="radio" name="sort" id="oldest" onClick={() => sortByPrice('published', -1)} />
+                    <input type="radio" name="sort" id="oldest" onClick={() => sortProducts('published', -1)} />
                     <label htmlFor='oldest'>Oldest product</label>
                 </div>
 
             </div>
             <section className="container">
                 <aside>
-                    <h3 className="category_heading">Category</h3>
-                    <div className="sub_categories">
-                        <p>sub category</p>
-                        <p>sub category</p>
-                        <p>sub category</p>
-                        <p>sub category</p>
-                    </div>
-                    <h3 className="category_heading">Category</h3>
-                    <div className="sub_categories">
-                        <p>sub category</p>
-                        <p>sub category</p>
-                        <p>sub category</p>
-                        <p>sub category</p>
-                    </div>
-                    <h3 className="category_heading">Category</h3>
-                    <div className="sub_categories">
-                        <p>sub category</p>
-                        <p>sub category</p>
-                        <p>sub category</p>
-                        <p>sub category</p>
-                    </div>
-
+                    {categories.map(sub => {
+                        return <Category key={sub.id} title={sub.categoryName} subCategories={sub.subCategories} />
+                    })}
                 </aside>
                 <main>
                     {products.length !== 0 ? (<section className="products">
